bddb221d7bd8c149994cbfd01bbd592133203401
arpitmoliya <arpitmoliya153@gmail.com>
2023-04-06 00:33:25 +0530

    feat: add teacher class

---
+++ Week-4/exercise-4.2/object.js
@@ -0,0 +1,29 @@
+/*Create an object called Teacher derived from the Person class,
+ and implement a method called teach which receives a string called subject, and prints out:
+ 
+ [teacher's name]is now teaching[subject]
+ 
+*/
+
+var Person = function () {};
+
+Person.prototype.initialize = function (name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+// TODO: create the class Teacher and a method teach\
+class Teacher extends Person {
+  constructor(value) {
+    super(value);
+  }
+
+  teach(subject) {
+    console.log(`${this.name} is now teaching ${subject}`);
+  }
+}
+
+var him = new Teacher();
+
+him.initialize("Adam", 45);
+him.teach("Inheritance");
