1ff0632368eb434b64569f152bc6d2af99e06fd3
arpitmoliya <arpitmoliya153@gmail.com>
2023-08-24 11:18:54 +0530

    task-3: convert Book component into pure component

--- Week-16/Assignment-2/problem-1to6/src/components/Book.js
+++ Week-16/Assignment-2/problem-1to6/src/components/Book.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-const Book = (props) => {
-  return (
-    <div className="book">
-      <h3>{props.title}</h3>
-      <p>Author: {props.author}</p>
-      <p>Year: {props.year}</p>
-    </div>
-  );
-};
+class Book extends PureComponent {
+  render(props) {
+    return (
+      <div className="book">
+        <h3>{props.title}</h3>
+        <p>Author: {props.author}</p>
+        <p>Year: {props.year}</p>
+      </div>
+    );
+  }
+}
 
 export default Book;
