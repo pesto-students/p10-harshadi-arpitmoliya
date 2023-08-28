06c3be7731162f161a5766d52d57061bf84afafb
arpitmoliya <arpitmoliya153@gmail.com>
2023-08-28 18:36:32 +0530

    feat: create env file for api keys

--- Week-17/Assignment-5/advance-medium-level/app.js
+++ Week-17/Assignment-5/advance-medium-level/app.js
@@ -4,9 +4,6 @@ const weatherRoutes = require("./routes/weather.routes");
 const app = express();
 app.use(express.json());
 
-const API_KEY = "bec04617b1be4d018f9101810232808";
-const API_URL = "http://api.weatherapi.com/v1";
-
 app.use(weatherRoutes);
 
 const PORT = 3000;
