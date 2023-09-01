bbd492acfe170f700a477d1990664a9f931fb8e0
arpitmoliya <arpitmoliya153@gmail.com>
2023-09-01 19:57:59 +0530

    feat: add middleware for routes

--- Week-17/Assignment-6/server.js
+++ Week-17/Assignment-6/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const assetRoutes = require("./app/routes/assets.routes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(assetRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
