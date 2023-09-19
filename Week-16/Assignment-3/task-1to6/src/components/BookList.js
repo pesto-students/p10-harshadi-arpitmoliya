0641bb084a182ea7a7f3f6850c089eca26eb24f8
arpitmoliya <arpitmoliya153@gmail.com>
2023-09-19 12:12:44 +0530

    feat: make this class component to functional component

--- Week-16/Assignment-3/task-1to6/src/components/BookList.js
+++ Week-16/Assignment-3/task-1to6/src/components/BookList.js
@@ -1,31 +1,22 @@
-import React, { Component } from "react";
-import Book from "./Book";
-class BookList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      books: [
-        { title: "Book 1", author: "Author 1", year: 2020 },
-        { title: "Book 2", author: "Author 2", year: 2018 },
-        { title: "Book 3", author: "Author 3", year: 2022 },
-      ],
-    };
-  }
+import React from "react";
 
-  render() {
-    return (
-      <div>
-        <h2>List of Books</h2>
-        <ul>
-          {this.state.books.map((book, index) => (
-            <li key={index}>
-              <Book title={book.title} author={book.author} year={book.year} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function BookList({ books }) {
+  return (
+    <div>
+      <h2>List of Books</h2>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            <div>
+              <h3>{book.title}</h3>
+              <p>Author: {book.author}</p>
+              <p>Year: {book.year}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default BookList;
