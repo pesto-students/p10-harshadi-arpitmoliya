3b1577ac8151c9b2cf75dc2863bb03cde489cc7a
arpitmoliya <arpitmoliya153@gmail.com>
2023-09-26 17:19:31 +0530

    feat: add delete functionality for books

--- Week-16/Assignment-3/task-1to6/src/components/BookList.js
+++ Week-16/Assignment-3/task-1to6/src/components/BookList.js
@@ -1,19 +1,27 @@
-import React from "react";
+function BookList({ books, onDelete }) {
+  const handleDelete = (index) => {
+    onDelete(index);
+  };
 
-function BookList({ books }) {
   return (
-    <div>
+    <div className="BookList">
       <h2>List of Books</h2>
       {books.length === 0 ? (
-        <p>The book list is empty</p>
+        <p>The book list is currently empty.</p>
       ) : (
         <ul>
           {books.map((book, index) => (
-            <li key={index}>
+            <li key={index} className="BookItem">
               <div>
                 <h3>{book.title}</h3>
                 <p>Author: {book.author}</p>
                 <p>Year: {book.year}</p>
+                <button
+                  className="DeleteButton"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
               </div>
             </li>
           ))}
