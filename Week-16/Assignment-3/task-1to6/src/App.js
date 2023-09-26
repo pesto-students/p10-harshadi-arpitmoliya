8ac5dabb81cda532520a6c284e3340c010e6ebe6
arpitmoliya <arpitmoliya153@gmail.com>
2023-09-26 17:20:33 +0530

    feat: create books array and function for add book and delete book

--- Week-16/Assignment-3/task-1to6/src/App.js
+++ Week-16/Assignment-3/task-1to6/src/App.js
@@ -1,22 +1,56 @@
+// App.js
 import React, { useState } from "react";
-import BookForm from "./components/BookForm";
-import BookList from "./components/BookList";
+import "./App.css"; // Import your CSS file here
+import BookForm from "../src/components/BookForm";
+import BookList from "../src/components/BookList";
+import BookDetail from "../src/components/BookDetail";
 
 function App() {
   const [books, setBooks] = useState([
-    { title: "Book 1", author: "Author 1", year: 2020 },
-    { title: "Book 2", author: "Author 2", year: 2018 },
-    { title: "Book 3", author: "Author 3", year: 2022 },
+    {
+      id: 1,
+      title: "Book 1",
+      author: "Author 1",
+      year: 2020,
+      description: "A great book about something interesting.",
+      genre: "Fiction",
+    },
+    {
+      id: 2,
+      title: "Book 2",
+      author: "Author 2",
+      year: 2018,
+      description: "Another interesting book worth reading.",
+      genre: "Non-fiction",
+    },
+    {
+      id: 3,
+      title: "Book 3",
+      author: "Author 3",
+      year: 2022,
+      description: "A new book for the new generation.",
+      genre: "Sci-Fi",
+    },
   ]);
 
   const addBook = (newBook) => {
     setBooks([...books, newBook]);
   };
 
+  const handleDelete = (index) => {
+    const updatedBooks = [...books];
+    updatedBooks.splice(index, 1);
+    setBooks(updatedBooks);
+  };
+
   return (
     <div className="App">
+      <h1>My Book List</h1>
       <BookForm onAddBook={addBook} />
-      <BookList books={books} />
+      <BookList books={books} onDelete={handleDelete} />
+      {books.map((book, index) => (
+        <BookDetail key={index} book={book} />
+      ))}
     </div>
   );
 }
