d614beaa84a72fd581e58ca512e3d5b72ac75643
arpitmoliya <arpitmoliya153@gmail.com>
2023-09-26 17:19:04 +0530

    feat: create formComponent for adding form

--- Week-16/Assignment-3/task-1to6/src/components/BookForm.jsx
+++ Week-16/Assignment-3/task-1to6/src/components/BookForm.jsx
@@ -1,83 +1,75 @@
-import React, { Component } from "react";
+import React,{ useState} from "react";
 
-class BookForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-      author: "",
-      year: "",
-    };
+function BookForm(onAddBook){
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    year: "",
+  })
+
+  const handleInputChange = (e)=> {
+    const {name,value} = e.target
+    setFormData({...formData,[name]:value})
+  } 
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+
+    const {title,author,year} = formData
+
+    if (title&author&year) {
+      const newBook = {
+        title,
+        author,
+        year: parseInt(year)
+      }
+
+      onAddBook(newBook)
+
+      setFormData({ title: '', author: '', year: '' });
+    }
   }
 
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const { title, author, year } = this.state;
-
-    const newBook = { title, author, year };
-
-    this.props.onAddBook(newBook);
-
-    this.setState({
-      title: "",
-      author: "",
-      year: "",
-    });
-  };
-
-  render() {
-    const { title, author, year } = this.state;
-
-    return (
-      <div>
-        <h2>Add new book</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={title}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="author">Author:</label>
-            <input
-              type="text"
-              id="author"
-              name="author"
-              value={author}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="year">Year:</label>
-            <input
-              type="number"
-              id="year"
-              name="year"
-              value={year}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <button type="submit">Add Book</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={formData.author}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="year">Year:</label>
+          <input
+            type="number"
+            id="year"
+            name="year"
+            value={formData.year}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Add Book</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default BookForm
